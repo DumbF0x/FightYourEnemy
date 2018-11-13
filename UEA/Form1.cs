b462fe7f45cd54992527b5a82b67fe59a2c44d1e
DumbF0x <43910760+DumbF0x@users.noreply.github.com>
2018-11-13 10:37:05 +0100

    Add files via upload

---
+++ UEA/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UEA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UEA ovire = new UEA(10);
+
+            List<List<char>> o = ovire.Ustvari(5, 1, 10, 10, 10, 0.6);
+
+            string s = "";
+            for(int i=0; i<o.Count; i++)
+            {
+                for(int j=0; j<o[i].Count; j++)
+                {
+                    if (o[i][j] == ' ')
+                        s += '_';
+                    else
+                        s += o[i][j];
+                    s += " ";
+                }
+                    
+                s += "\n";
+            }
+
+            label1.Text = s;
+        }
+    }
+}
